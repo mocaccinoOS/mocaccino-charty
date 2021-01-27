3275e6d19797c86bc71eef7a11743c19fc0c3778
Ettore Di Giacinto <mudler@users.noreply.github.com>
2021-01-27 19:01:57 +0100

    Add luet-devkit to artifact-qa charty
    Also adds LUET_YES=true

--- testcharts/artifact-qa/templates/qa.sh
+++ testcharts/artifact-qa/templates/qa.sh
@@ -2,13 +2,13 @@
 set -ex
 export TMPDIR=$PWD/tmp
 export LUET_NOLOCK=true
-
+export LUET_YES=true
 {{$debug:=.Values.debug}}
 {{$rootfs:=.Values.rootfs_dir}}
 {{$packagesdir:=.Values.packages_dir}}
 
 
-./bin/luet install --config config.yaml system/luet-extensions {{- if not $debug }}>/dev/null 2>&1 {{- end }}
+./bin/luet install --config config.yaml system/luet-extensions system/luet-devkit {{- if not $debug }}>/dev/null 2>&1 {{- end }}
 
 export ROOT_DIR={{$packagesdir}}
 {{- if .Values.features.verify_checksum }}
